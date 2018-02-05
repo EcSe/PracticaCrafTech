d8ee1f24cc281d3276a51fb6f714f6d37e85bbdf
ELVIN-PC <ecse88@gmail.com>
2018-02-05 00:33:45 -0500

    Previa  al examen

---
+++ CraftTechApp/CapaDAO/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static SqlConnection ConectarBD()
+        {
+         String cadena = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+           SqlConnection conexion = new SqlConnection(cadena);
+           return conexion;
+        }
+    }
+}
---
+++ CraftTechApp/CapaNegocio/ProductoDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+using Entidades;
+
+namespace CapaNegocio
+{
+    public class ProductoDAO
+    {
+        
+        SqlConnection con = Conexion.ConectarBD();
+        
+        public DataSet Listar()
+        {
+            SqlCommand cmd = new SqlCommand("spcListarProducto",con );
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
